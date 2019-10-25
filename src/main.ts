0a38f33e1df041947d2a97b7aae61592a5224170
zeze.li <lizeze.boy@gmail.com>
2019-10-25 17:32:35 +0800

    'add'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import Vue from 'vue'
-import Index from "@/Index.vue";
+import Home from "@/Home.vue";
 
 Vue.config.productionTip = false
 import HttpRequest from "@/request/HttpRequest";
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
+import VueRouter from 'vue-router'
+import routes from "@/router/index";
+
+const router = new VueRouter({
+    routes, // (缩写) 相当于 routes: routes
+    mode: 'history',
+})
+
+Vue.use(VueRouter);
+
 Vue.prototype.$service = HttpRequest;
 Vue.use(Element, {size: 'small', zIndex: 3000})
 new Vue({
-    render: h => h(Index),
+    render: h => h(Home),
+    router
+
 }).$mount('#app')
