9c51a9add8f5d67ccf7c0d3f8c6339e4a7fd7ab7
zeze.li <lizeze.boy@gmail.com>
2019-10-29 23:34:50 +0800

    '增加面包屑'

---
+++ src/view/system/User.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+ wdwd
+
+    </div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Breadcrumb from "@/view/home/components/Breadcrumb.vue";
+import Vue from 'vue';
+
+@Component({
+    components:{
+        Breadcrumb
+    }
+})
+export default class Order extends Vue {
+
+
+
+}
+</script>
+<style scoped>
+
+</style>
